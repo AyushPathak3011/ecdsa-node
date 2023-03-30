955a66829a6cfb3ed612749016d14d8d9959aa1e
AyushPathak3011 <ayushpathak3011@gmail.com>
2023-03-30 23:00:06 +0530

    Create keys.js

---
+++ client/src/keys.js
@@ -0,0 +1,22 @@
+export const KEYS = [
+  {
+    Private_key:
+      "eaa5deb6032cacbd5c662f93a8f70ee5b0498a850cfd981c441859a741ef3115",
+    Address: "1519646db8df3734adfacde8aa8c185a5126e747",
+  },
+  {
+    Private_key:
+      "c67fa46b0f06218933fb89d77d29210f7f0b5e7d226efb354124e5233fcfd448",
+    Address: "a69994f24cdbf972a5d3aab24371c4cc41301a10",
+  },
+
+  {
+    Private_key:
+      "1d8caee28368c6e28f1abd0b4bcd39343fe3ff982effd6d933ba72ec13828ff6",
+    Address: "d0cb79f616eb7388ce72582a313c74a74a520c14",
+  },
+];
+
+// d0cb79f616eb7388ce72582a313c74a74a520c14
+//  a69994f24cdbf972a5d3aab24371c4cc41301a10
+// 1d8caee28368c6e28f1abd0b4bcd39343fe3ff982effd6d933ba72ec13828ff6
